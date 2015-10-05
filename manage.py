4e7555b007bd617588decb019261d5da4ec6813a
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-05 16:36:14 +0300

    Added App Bootstrap to settings.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+s#!/usr/bin/env python
 import os
 import sys
-
+#Adding my app to manage.py
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pretty.settings")
 
